dfc2ed6fc22ca32d300442303835f6bcead29f22
damien.lignac <damien.lignac@cgi.com>
2022-11-08 17:58:09 +0100

    Init with search link algo

---
+++ src/test/java/fr/ligo/ligo/LigoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ligo.ligo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
